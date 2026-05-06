a3d75dd0c91b217eb214e82fd82e15cc128400a1
Craig Linenger <craig.linenger@commissionsinc.com>
2026-05-06 15:43:58 -0400

    Update run.sh

--- plexamp/run.sh
+++ plexamp/run.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/with-contenv bashio
 
+# Read config from Home Assistant
 CLAIM_TOKEN=$(bashio::config 'claim_token')
 PLAYER_NAME=$(bashio::config 'player_name')
 
+# Export environment variables for Plexamp
 if [ -n "$CLAIM_TOKEN" ]; then
-  export PLEXAMP_CLAIM_TOKEN="$CLAIM_TOKEN"
+    export PLEXAMP_CLAIM_TOKEN="$CLAIM_TOKEN"
 fi
 
 export PLEXAMP_PLAYER_NAME="$PLAYER_NAME"
 
-exec /init
+bashio::log.info "Starting Plexamp add-on..."
+
+# IMPORTANT:
+# We directly exec the container's real entrypoint
+# because ghcr.io/anatosun/plexamp is NOT a Home Assistant base image
+
+exec /init || exec plexamp || exec node /app/index.js
